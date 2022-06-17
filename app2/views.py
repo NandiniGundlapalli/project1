33c65b56a9afdeec31f475b99aecff909ff85757
NandiniGundlapalli <nandini.gundlapalli1513@gmail.com>
2022-06-17 19:14:55 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def kohli(request):
+    return HttpResponse('kohli is the worlds best batsman')
